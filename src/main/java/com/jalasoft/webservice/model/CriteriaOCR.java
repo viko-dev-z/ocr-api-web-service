f284a44e6427101f836f0cc5fbc32528c2a36e74
acrtson <acrtson@gmail.com>
2019-09-21 15:12:21 -0400

    no message

--- src/main/java/com/jalasoft/webservice/model/CriteriaOCR.java
+++ src/main/java/com/jalasoft/webservice/model/CriteriaOCR.java
@@ -1,4 +1,25 @@
+/*
+ *
+ *  Copyright (c) 2019 Jalasoft.
+ *
+ *  This software is the confidential and proprietary information of Jalasoft.
+ *  ("Confidential Information"). You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into
+ *  with Jalasoft.
+ * /
+ */
+
 package com.jalasoft.webservice.model;
 
-public class CriteriaOCR {
+import java.util.List;
+
+public class CriteriaOCR extends Criteria {
+
+    File imageFile;
+
+    public CriteriaOCR() {
+        super();
+
+    }
 }
