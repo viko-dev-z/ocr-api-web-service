82950041fce6001c1723a96b9c02a76a3b387852
Victor Paredes <vico.sistemas@gmail.com>
2019-09-21 14:46:09 -0400

    Added logic to use POST endpoint /api/v1/ocr/processImage

--- src/main/java/com/jalasoft/webservice/controller/ProcessImageRestController.java
+++ src/main/java/com/jalasoft/webservice/controller/ProcessImageRestController.java
@@ -12,5 +12,40 @@
 
 package com.jalasoft.webservice.controller;
 
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+@RestController
+@RequestMapping("/api/v1")
 public class ProcessImageRestController {
+
+    public ProcessImageRestController() {
+    }
+
+    @RequestMapping("/ocr/processImage")
+    @PostMapping
+    public String extractText(
+            @RequestParam(value="file") MultipartFile file,
+            @RequestParam(value="language", defaultValue="eng") String language) {
+        try{
+        String filePath="D:/temp/_image_"+file.getOriginalFilename();
+        Path location = Paths.get(filePath);
+        Files.copy(file.getInputStream(),location);
+// Alex uncomment these lines once you implement OCRModel.java logic
+//      OCRModel OCR = new OCRModel(filePath, language);
+//      String result  = OCR.extract();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//      retun result;
+        return "Done..";
+    }
 }
