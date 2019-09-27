390e7aefc585686a7143a87dd84caa94c12dc833
acrtson <acrtson@gmail.com>
2019-09-27 18:27:28 -0400

    adding the DBManager class

---
+++ src/main/java/com/jalasoft/webservice/model/DBManager.java
@@ -0,0 +1,31 @@
+/*
+ *
+ *  Copyright (c) 2019 Jalasoft.
+ *
+ *  This software is the confidential and proprietary information of Jalasoft.
+ *  ("Confidential Information"). You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into
+ *  with Jalasoft.
+ * /
+ */
+
+package com.jalasoft.webservice.model;
+
+import com.jalasoft.webservice.database.DBQuery;
+
+public class DBManager {
+    DBQuery dbQuery;
+
+     public DBManager() {
+         dbQuery = new DBQuery();
+     }
+
+     public void addFile(String  checksum, String path){
+         dbQuery.insert(checksum, path);
+     }
+
+     public String getPath(String checksum){
+         return  dbQuery.getPath(checksum);
+     }
+}
