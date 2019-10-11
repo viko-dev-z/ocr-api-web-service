4be32ac276f6b9c6ee291ff5790cacab966e555f
acrtson <acrtson@gmail.com>
2019-10-11 09:43:18 -0400

    added the code to create the userTable to use tokens

--- src/main/java/com/jalasoft/webservice/database/ConnectionDB.java
+++ src/main/java/com/jalasoft/webservice/database/ConnectionDB.java
@@ -29,6 +29,7 @@ public class ConnectionDB {
     private static Connection conn;
     // Constant to assign table name
     static final String TABLE_NAME = "fileState";
+    static final String USER_TABLE_NAME = "userTable";
 
     /**
      * Private constructor to prevent direct instantiation.
@@ -60,6 +61,14 @@ public class ConnectionDB {
                     + "( Id integer primary key, " +
                       "Checksum varchar(32), " +
                       "Path varchar (250) );");
+            state = conn.createStatement();
+            state.execute("create table if not exists " +
+                     USER_TABLE_NAME +
+                     "( Id integer primary key, "+
+                     "user varchar(32) UNIQUE, " +
+                     "password varchar(8), " +
+                     "role varchar(16), " +
+                     "email varchar(16) UNIQUE );");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
