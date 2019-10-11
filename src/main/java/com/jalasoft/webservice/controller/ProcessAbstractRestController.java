d8cebd56cf1086c1addb60f305fabce66421929e
acrtson <acrtson@gmail.com>
2019-10-10 21:16:07 -0400

    moved the processResponce method to the ProcessAbstractRestController class

--- src/main/java/com/jalasoft/webservice/controller/ProcessAbstractRestController.java
+++ src/main/java/com/jalasoft/webservice/controller/ProcessAbstractRestController.java
@@ -13,9 +13,46 @@
 package com.jalasoft.webservice.controller;
 
 import com.jalasoft.webservice.model.DBManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class ProcessAbstractRestController {
     DBManager dbm = new DBManager();
     ResponsesSupported myResponses = ResponsesSupported.OK;
     Response jsonMessage;
+
+    /**
+     * Process a Response Body according to Enum value
+     * assigned in processImage method.
+     */
+    public ResponseEntity processResponse() {
+        ResponseEntity response = null;
+        switch(myResponses) {
+            case OK:
+                response = ResponseEntity
+                        .status(HttpStatus.OK)
+                        .header("Content-Type", "application/json; charset=UTF-8")
+                        .body(jsonMessage.getJSON());
+                break;
+            case LANG_UNSUPPORTED:
+                jsonMessage = new ResponseErrorMessage();
+                jsonMessage.setCode("400");
+                jsonMessage.setMessage("Not a Valid Language property");
+                response = ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .header("Content-Type", "application/json; charset=UTF-8")
+                        .body(jsonMessage.getJSON());
+                break;
+            case FILE_UNSUPPORTED:
+                jsonMessage = new ResponseErrorMessage();
+                jsonMessage.setCode("415");
+                jsonMessage.setMessage("Not a Valid PDF Format");
+                response = ResponseEntity
+                        .status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                        .header("Content-Type", "application/json; charset=UTF-8")
+                        .body(jsonMessage.getJSON());
+                break;
+        }
+        return response;
+    }
 }
--- src/main/java/com/jalasoft/webservice/controller/ProcessPdfRestController.java
+++ src/main/java/com/jalasoft/webservice/controller/ProcessPdfRestController.java
@@ -123,38 +123,6 @@ public class ProcessPdfRestController extends ProcessAbstractRestController {
         return processResponse();
     }
 
-    /**
-     * Process a Response Body according to Enum value
-     * assigned in processImage method.
-     */
-    private ResponseEntity processResponse() {
-        ResponseEntity response = null;
-        switch(myResponses) {
-            case OK:
-                response = ResponseEntity
-                        .status(HttpStatus.OK)
-                        .header("Content-Type", "application/json; charset=UTF-8")
-                        .body(jsonMessage.getJSON());
-                break;
-            case LANG_UNSUPPORTED:
-                jsonMessage = new ResponseErrorMessage();
-                jsonMessage.setCode("400");
-                jsonMessage.setMessage("Not a Valid Language property");
-                response = ResponseEntity
-                        .status(HttpStatus.BAD_REQUEST)
-                        .header("Content-Type", "application/json; charset=UTF-8")
-                        .body(jsonMessage.getJSON());
-                break;
-            case FILE_UNSUPPORTED:
-                jsonMessage = new ResponseErrorMessage();
-                jsonMessage.setCode("415");
-                jsonMessage.setMessage("Not a Valid PDF Format");
-                response = ResponseEntity
-                        .status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                        .header("Content-Type", "application/json; charset=UTF-8")
-                        .body(jsonMessage.getJSON());
-                break;
-        }
-        return response;
-    }
+
+
 }
