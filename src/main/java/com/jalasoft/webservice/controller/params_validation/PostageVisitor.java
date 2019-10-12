25da35c00208750ac166950cbb692a271ba87298
acrtson <acrtson@gmail.com>
2019-10-11 22:27:29 -0400

    changing the type String to StringBuilder for validationResult variable. added the message variable to construct the error message and then add it to validationResult and to ParamInvalidException

--- src/main/java/com/jalasoft/webservice/controller/params_validation/PostageVisitor.java
+++ src/main/java/com/jalasoft/webservice/controller/params_validation/PostageVisitor.java
@@ -11,13 +11,16 @@
  */
 
 package com.jalasoft.webservice.controller.params_validation;
-
 import com.jalasoft.webservice.error_handler.ParamsInvalidException;
-
 import static java.lang.Integer.parseInt;
 
 public class PostageVisitor implements Visitor {
-    private String validationResult = "";
+    private StringBuilder validationResult;
+    private String message;
+
+    public PostageVisitor(){
+        validationResult = new StringBuilder();
+    }
 
     public void visit(GenericParam param) throws ParamsInvalidException {
         validateCommonData(param);
@@ -27,13 +30,15 @@ public class PostageVisitor implements Visitor {
     public void visit(ChecksumParam checksumParam) throws ParamsInvalidException {
         validateCommonData(checksumParam);
         if (checksumParam.getValue().toString().length() != 32 ){
-            validationResult += checksumParam.getName() + " has invalid length";
-            throw new ParamsInvalidException("The " + checksumParam.getName() + " has an invalid length\n");
+            message = "The " + checksumParam.getName() + " has an invalid length. \n";
+            validationResult.append(message);
+            throw new ParamsInvalidException(message);
         }
 
         if (!checksumParam.getValue().toString().matches("-?[0-9a-fA-F]+")){
-            validationResult += checksumParam.getName() + " has invalid characters: " + checksumParam.getValue().toString();
-            throw new ParamsInvalidException("The " + checksumParam.getName() + " has an invalid characters\n");
+            message = "The " + checksumParam.getName() + " has an invalid characters: " + checksumParam.getValue().toString() + "\n";
+            validationResult.append(message);
+            throw new ParamsInvalidException(message);
         }
     }
 
@@ -44,26 +49,32 @@ public class PostageVisitor implements Visitor {
         try {
             number = parseInt(intParam.getValue().toString());
         } catch (NumberFormatException n){
-            throw new ParamsInvalidException("The " + intParam.getName() + " is an invalid number character");
+            message = "The " + intParam.getName() + " is and invalid number character: " + intParam.getValue().toString();
+            validationResult.append(message);
+            throw new ParamsInvalidException(message);
         }
 
         if (number < 0){
-            throw new ParamsInvalidException("The " + intParam.getName() + " must be greater than 0");
+            message = "The " + intParam.getName() + " must be greater than 0";
+            validationResult.append(message);
+            throw new ParamsInvalidException(message);
         }
     }
 
     String getValidationResult(){
-        return validationResult;
+        return validationResult.toString();
     }
 
     private void validateCommonData(AbstractParam common) throws ParamsInvalidException {
         if (common.getValue().toString().isEmpty()){
-            validationResult += "The " + common.getName() + " is empty \n";
-            throw new ParamsInvalidException("The " + common.getName() + " is empty \n");
+            message = "The " + common.getName() + " is empty \n";
+            validationResult.append(message);
+            throw new ParamsInvalidException(message);
         }
         if (common.getValue() == null){
-            validationResult += "The " + common.getName() + " is null \n";
-            throw new ParamsInvalidException("The " + common.getName() + " is null \n");
+            message = "The " + common.getName() + " is null \n";
+            validationResult.append(message);
+            throw new ParamsInvalidException(message);
         }
     }
 }
