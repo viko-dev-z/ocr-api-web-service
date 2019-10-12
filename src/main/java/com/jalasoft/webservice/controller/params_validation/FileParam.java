45103ecf09d7e157cfdea1f9614bad78f18c343e
acrtson <acrtson@gmail.com>
2019-10-12 00:28:26 -0400

    FileParam allows to validate the file type in the Visitor design patern

---
+++ src/main/java/com/jalasoft/webservice/controller/params_validation/FileParam.java
@@ -0,0 +1,26 @@
+/*
+ *
+ *  Copyright (c) 2019 Jalasoft.
+ *
+ *  This software is the confidential and proprietary information of Jalasoft.
+ *  ("Confidential Information"). You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into
+ *  with Jalasoft.
+ * /
+ */
+
+package com.jalasoft.webservice.controller.params_validation;
+
+import com.jalasoft.webservice.error_handler.ParamsInvalidException;
+
+public class FileParam extends AbstractParam {
+    public FileParam(String name, Object value){
+        setName(name);
+        setValue(value);
+    }
+
+    public void accept(Visitor visitor) throws ParamsInvalidException {
+        visitor.visit(this);
+    }
+}
