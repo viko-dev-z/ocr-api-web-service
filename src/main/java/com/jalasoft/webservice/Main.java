e05c2ed0174fb3f4f3616e3d84022f07198e9d93
acrtson <acrtson@gmail.com>
2019-10-18 08:45:29 -0400

    Using WebFilter

--- src/main/java/com/jalasoft/webservice/Main.java
+++ src/main/java/com/jalasoft/webservice/Main.java
@@ -14,8 +14,10 @@ package com.jalasoft.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
---
+++ src/main/java/com/jalasoft/webservice/controller/filters/TokeFilter.java
@@ -0,0 +1,47 @@
+/*
+ *
+ *  Copyright (c) 2019 Jalasoft.
+ *
+ *  This software is the confidential and proprietary information of Jalasoft.
+ *  ("Confidential Information"). You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into
+ *  with Jalasoft.
+ * /
+ */
+
+package com.jalasoft.webservice.controller.filters;
+
+import com.jalasoft.webservice.controller.Cache;
+import org.apache.catalina.filters.ExpiresFilter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"})
+public class TokeFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse res = (ExpiresFilter.XHttpServletResponse)response;
+
+        if (Cache.getInstance().isValid(req.getHeader("Authorization"))){
+            chain.doFilter(request, response);
+        } else {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Token");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
