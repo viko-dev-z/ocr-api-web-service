563da283c4137c13b973872b96d01d76c99a617a
acrtson <acrtson@gmail.com>
2019-10-20 12:54:39 -0400

    added the property file path

--- src/main/java/com/jalasoft/webservice/common/StandardValues.java
+++ src/main/java/com/jalasoft/webservice/common/StandardValues.java
@@ -16,6 +16,7 @@ public final class StandardValues {
     public static final String EMPTY_STRING = "";
     public static final String MIME_TYPE_IMAGE = "image";
     public static final String REGULAR_EXPRESSION_HEX_NUMBERS = "-?[0-9a-fA-F]+";
+    public static final String PROPERTY_FILE_PATH = "imagePath";
     static final String MIME_TYPE_APPLICATION_PDF = "application/pdf";
     static final String MD5 = "MD5";
     static final String FORMAT = "%02x";
