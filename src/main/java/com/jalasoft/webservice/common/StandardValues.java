7f005b147532a0e8a1aaac2731116356e34c2654
acrtson <acrtson@gmail.com>
2019-10-20 13:47:03 -0400

    Added the constants CHECKSUM_NAME and FILE_NAME

--- src/main/java/com/jalasoft/webservice/common/StandardValues.java
+++ src/main/java/com/jalasoft/webservice/common/StandardValues.java
@@ -17,6 +17,8 @@ public final class StandardValues {
     public static final String MIME_TYPE_IMAGE = "image";
     public static final String REGULAR_EXPRESSION_HEX_NUMBERS = "-?[0-9a-fA-F]+";
     public static final String PROPERTY_FILE_PATH = "imagePath";
+    public static final String CHECKSUM_NAME = "checksum";
+    public static final String FILE_NAME = "file";
     static final String MIME_TYPE_APPLICATION_PDF = "application/pdf";
     static final String MD5 = "MD5";
     static final String FORMAT = "%02x";
