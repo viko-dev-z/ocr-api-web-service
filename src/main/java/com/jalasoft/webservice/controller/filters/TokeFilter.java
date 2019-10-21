9e72fe83d66d8805233e890e8a0139bd9563e53c
acrtson <acrtson@gmail.com>
2019-10-21 08:53:59 -0400

    cleaning the code

--- src/main/java/com/jalasoft/webservice/controller/filters/TokeFilter.java
+++ src/main/java/com/jalasoft/webservice/controller/filters/TokeFilter.java
@@ -35,7 +35,7 @@ public class TokeFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest)request;
         HttpServletResponse res = (HttpServletResponse)response;
         String url = req.getRequestURL().toString();
-boolean test = url.contains("/file");
+
         if (url.contains("/login") || url.contains("/file") || Cache.getInstance().isValid(req.getHeader(HttpHeaders.AUTHORIZATION).split(StandardValues.SPACE)[1])){
             chain.doFilter(request, response);
         } else {
